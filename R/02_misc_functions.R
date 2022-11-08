706a4415f1ffcd22de584a77068482db6ce0b96b
Vanessa Klaas <klav@zhaw.ch>
2022-11-08 15:13:00 +0100

    closes 4: install and config for libraries
    * README.md: renv removed
    * libraries.R: config-constants added, systematic loading of libraries
    * theme_definition: boostrap config only if USE_BOOTSTRAP is TRUE

--- R/02_misc_functions.R
+++ R/02_misc_functions.R
@@ -1,5 +1,5 @@
-# Misc functions
-# functions that are helpers and can be useful in several contexts
+#' Misc functions
+#' functions that are helpers and can be useful in several contexts
 
 
 #' create an id out of the key and the redcap record_id
@@ -16,6 +16,17 @@ create_id <- function(key, record_id) {
   return(as.integer(result))
 }
 
+#' reverse_item
+#' vectorized function to reverse on item or a vector of items
+#' useful for questionnaires where some items have a reversed scale
+#'
+#' @param v the value that should be reversed
+#' @param max_value the maximum value of the scale
+#'
+#' @return
+#' @export
+#'
+#' @examples
 reverse_item <- function(v, max_value) {
   return(max_value - v)
 }
--- app.R
+++ app.R
@@ -1,12 +1,19 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-# You can publish the app to RS Connect through the button next to the run button.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+#' *****************************************************************************
+#' This is a Shiny web application. You can run the application by clicking
+#' the 'Run App' button above.
+#' You can publish the app to RS Connect through the 'publish'-button.
+#' It sits next to the run button.
+#' 
+#' How to build your own app.
+#' * Build your ui in the ui.R script.
+#' * Build your server functionality in the server.R script.
+#' * Define more complex functions in the scripts provided in the R-folder.
+#'
+#' Find out more about building applications with Shiny here:
+#'
+#'    http://shiny.rstudio.com/
+#'    
+#' *****************************************************************************
 
 
 # start the app
